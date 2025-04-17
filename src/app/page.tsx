643e030322c3dbce419d66b136e3dce9e87c9405
Hxrshxl <harshal2629@gmail.com>
2025-04-17 19:26:48 +0530

    dep. error6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import Navbar from "@/components/global/Navbar"
 import Hero from "@/components/global/Hero"
 import Features from "@/components/global/Features"
 import HowItWorks from "@/components/global/HowItWorks"
-import Pricing from "@/components/global/Pricing"
 import CTASection from "@/components/global/CTASection"
 import Footer from "@/components/global/Footer"
-import Testimonials from "@/components/global/Pricing"
+import Testimonials from "@/components/global/Testinomials"
+import Pricing from "@/components/global/Pricing"
 
 export default function Home() {
   return (
